cd5db71be6d83a210c3a658a3dafcdc984d82da4
Diego Martinez <diegomart_2000@yahoo.com>
2015-10-29 22:55:14 -0300

    Updated to 5.x

--- canvas/cloud/main.js
+++ canvas/cloud/main.js
@@ -1,7 +1,20 @@
-require('cloud/app.js');
+// These two lines are required to initialize Express in Cloud Code.
+var express = require('express');
+var app = express();
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+// Global app configuration section
+app.set('views', 'cloud/views');  // Specify the folder to find templates
+app.set('view engine', 'jade');    // Set the template engine
+app.use(express.bodyParser());    // Middleware for reading request body
+
+//Main application entry point
+app.get('/', function(req, res) {
+  res.render('hello');
 });
+
+//Application partials
+app.get('/partials/:name', function(req, res) {
+  res.render('partials/' + req.params.name);
+});
+
+app.listen();
